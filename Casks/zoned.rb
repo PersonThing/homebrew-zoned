ced5d6a92b2c50ba99888eb51cba494eee45fdb9
Tim Schottler <tschottler@gmail.com>
2026-03-02 18:27:06 -0600

    chore: bump to v0.2.0

--- Casks/zoned.rb
+++ Casks/zoned.rb
@@ -1,6 +1,6 @@
 cask "zoned" do
-  version "0.1.0"
-  sha256 "81da00ee11a0b101e70446584e3e6f74e02f786a3019bcd1ebe016ab459ab969"
+  version "0.2.0"
+  sha256 "00847f103147f5006d550b4fa55360bc2fb1dc3e2cfa7f7eaff64bc83d81c94d"
 
   url "https://github.com/PersonThing/zoned/releases/download/v#{version}/Zoned-#{version}.zip"
   name "Zoned"
@@ -17,6 +17,7 @@ cask "zoned" do
 
   zap trash: [
     "~/Library/Preferences/com.zoned.app.plist",
+    "~/Library/Application Support/Zoned",
   ]
 
   caveats <<~EOS
